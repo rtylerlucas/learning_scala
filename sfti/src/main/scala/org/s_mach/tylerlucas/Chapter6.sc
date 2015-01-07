09aa8ca00be7ec9840e33792082dc794c7f8fba1
R Tyler Lucas <rtylerlucas@gmail.com>
2015-01-07 18:09:09 -0500

    Chp 6 Exercises and began Chp 7: Imports & Pkgs

--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter6.sc
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter6.sc
@@ -1,3 +1,5 @@
+import org.s_mach.tylerlucas.Chapter6.Reverse
+
 object Origin extends java.awt.Point
 //Ex. 3
 println(Origin)
--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter6.scala
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter6.scala
@@ -27,6 +27,29 @@ class Chapter6 {
       new Point(x,y)
     }
   }
+  //still needs to be tested from command line.
+  //need to learn best practice for importing and testing classes/objs
+  //from repl
+  object Reverse extends App {
+    var counter = 0
+    var current = ""
+    if(args.length>0){
+      for(i<-args.length-1 until 0 by -1) {
+       if(args(i)!=" "){
+         counter+= 1
+         current = args(i) + current
+       }
+        else{
+         counter = 0
+         print(current + " ")
+         current = ""
+       }
+      }
+      print(current)
+    }
+    else println("Empty Args!")
+
+  }
 
 
 }
