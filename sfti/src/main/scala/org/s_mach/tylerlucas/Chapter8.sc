fd2bb5cfbdf3f7abe834725bb96b4f8e0a37073a
R Tyler Lucas <rtylerlucas@gmail.com>
2015-01-13 11:14:59 -0500

    Chapter 8 Exercises

--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter8.sc
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter8.sc
@@ -1,7 +1,16 @@
-import org.s_mach.tylerlucas.CheckingAccount
-
+import org.s_mach.tylerlucas.{Bundle, SimpleItem, CheckingAccount}
+import java.util.Calendar
 val checking = new CheckingAccount(1000)
 checking.currentBalance
 checking.deposit(2)
 checking.currentBalance
 checking.withdraw(50)
+val date1 = Calendar.getInstance().getTime()
+val date2 = Calendar.getInstance().getTime()
+val mat = date2.compareTo(date1)
+
+val textbook = new SimpleItem(100, "Math and science book")
+val shoes = new SimpleItem(50, "Nike Sneakers")
+val bundle = new Bundle(textbook, shoes)
+bundle.description
+bundle.price
--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter8.scala
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter8.scala
@@ -12,7 +12,70 @@ class BankAccount(initialBalance: Double) {
   def deposit(amount: Double) = {balance += amount; balance}
   def withdraw(amount: Double) = {balance -= amount; balance}
 }
+//Ex. 1
 class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance) {
   override def deposit(amount: Double): Double = {balance -= 1; balance += amount; balance}
   override def withdraw(amount: Double): Double = {balance -= 1; balance -= amount; balance}
+}
+//Ex 2
+class SavingsAccount(initialBalance: Double) extends BankAccount(initialBalance) {
+  private var freeTransactions = 3
+  private val startDate = java.util.Calendar.getInstance().getTime()
+
+  override def deposit(amount: Double): Double = {
+    if(freeTransactions!=0) freeTransactions -= 1
+    else balance -= 1
+    balance += amount
+    balance
+  }
+
+  override def withdraw(amount: Double): Double = {
+    if(freeTransactions!=0) freeTransactions -= 1
+    else balance -= 1
+    balance -= amount
+    balance
+  }
+  def earnMonthlyInterest = {
+    balance = balance + (balance * 0.05)
+    freeTransactions = 3
+  }
+}
+//Ex 3
+class Animal {
+
+  def sleep = println("The Animal sleeps...")
+  def eat = println("The Animal eats...")
+}
+class Dog extends Animal {
+  override def sleep = println("The Dog sleeps...")
+  override def eat = println("The Dog eats puppy chow...")
+}
+//Ex. 4
+abstract class Item {
+  def price: Double
+  def description: String
+}
+class SimpleItem(override val price: Double, override val description: String) extends Item {
+
+}
+class Bundle(args: SimpleItem*) extends Item {
+  var price: Double = 0.0
+  var description = ""
+  for(arg <- args) { price += arg.price; description += ", " + arg.description }
+  def addItem(item: SimpleItem) = { price += item.price }
+}
+
+//Ex. 5
+class Point(val x: Int, val y: Int) {}
+class LabeledPoint(val label: String, override val x: Int, override val y: Int) extends Point(x,y){}
+
+//Ex. 6
+abstract class Shape{
+  def centerPoint
+}
+class Circle(val radius: Int) extends Shape{
+  val centerPoint = new Point(0,0)
+}
+class Rectangle(val width: Int, val height: Int) extends Shape {
+  val centerPoint = new Point(0,0)
 }
