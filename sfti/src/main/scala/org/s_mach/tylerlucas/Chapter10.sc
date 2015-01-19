878c20221d84fad364bd6af85704495300913769
R Tyler Lucas <rtylerlucas@gmail.com>
2015-01-19 17:09:39 -0500

    worked on Chp 10 Traits.

--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter10.sc
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter10.sc
@@ -1,7 +1,23 @@
 import java.awt.geom.Ellipse2D.Double
 
-import org.s_mach.tylerlucas.RectangleLike
+import org.s_mach.tylerlucas._
 
-//val egg = new Double(5,10,20,30) with RectangleLike
-//egg.translate(5,5)
-//egg.getX
+//Ex. 1 Testing
+val egg = new Double(5,10,20,30) with RectangleLike
+egg.getFrame
+egg.translate(5,5)
+egg.getX
+egg.getY
+
+//Ex. 2 Testing
+val orderedPt = new OrderedPoint
+//val oPt2 = new OrderedPoint(5,5)
+//orderedPt.compare(oPt2)
+
+
+//Ex. 4
+val ex4 = new Chapter10 with CryptoLogger
+ex4.encrypt("tyler")
+
+//Ex. 5
+//val pt = new java.awt.Point with PropertyChangeSupport
--- sfti/src/main/scala/org/s_mach/tylerlucas/Chapter10.scala
+++ sfti/src/main/scala/org/s_mach/tylerlucas/Chapter10.scala
@@ -6,18 +6,70 @@ package org.s_mach.tylerlucas
 class Chapter10 {
 
 }
+//Ex. 1
 trait RectangleLike {
-  def x: Double
-  def y: Double
-  def width: Double
-  def height: Double
+  def getX: Double
+  def getY: Double
+  def getWidth: Double
+  def getHeight: Double
+  def setFrame(x: Double, y: Double, width: Double, height: Double): Unit
 
-   def translate(dx: Int, dy: Int) = {
-    x += dx
-    y += dy
+  def translate(dx: Int, dy: Int) = {
+    setFrame(getX+dx, getY+dy, getWidth, getHeight)
   }
-   def grow(x: Int, y: Int) = {
-    height += y
-    width += x
+  def grow(dHeight: Int, dWidth: Int) = {
+    setFrame(getX,getY,getWidth+dWidth, getHeight+dHeight)
+
   }
+}
+
+//Ex. 2
+class OrderedPoint extends java.awt.Point with scala.math.Ordered[OrderedPoint]{
+
+  def compare(orderedPoint: OrderedPoint): Int = {
+    if( getX < orderedPoint.getX  || (getX == orderedPoint.getX && getY < orderedPoint.getY)) -1
+    else{
+      if(orderedPoint.getX == getX && orderedPoint.getY == getY) 0
+      else 1
+    }
+  }
+  def this(dx: Int, dy: Int){
+    this()
+    x = dx
+    y = dy
+  }
+}
+
+//Ex. 4
+trait Logged {
+  def log(msg: String)
+}
+trait CryptoLogger extends Logged {
+  private final val caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  private final val lower = "abcdefghijklmnopqrstuvwxyz"
+  val shift = 3
+  def encrypt(msg: String): String = {
+    var encrypted: String = ""
+    for(ch <- msg){
+        val x = (Math.max(caps.indexOf(ch), lower.indexOf(ch)))
+        encrypted += lower((x+shift) % 26)
+    }
+    encrypted
+  }
+  def decrypt(msg: String): String = {
+    var decrypted: String = ""
+    for(ch <- msg){
+      val x = (Math.max(caps.indexOf(ch), lower.indexOf(ch)))
+      decrypted += lower((x-shift) % 26)
+    }
+    decrypted
+  }
+  def log(msg: String) = {
+    println(encrypt(msg))
+  }
+}
+
+//Ex 5. working on it
+trait PropertyChangeSupport  {
+
 }
